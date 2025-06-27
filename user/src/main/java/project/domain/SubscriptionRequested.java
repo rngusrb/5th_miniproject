ea369b5adef0e259359f04b7f25da09fe8572a28
이유민 <lee_yumin@naver.com>
2025-06-27 15:48:27 +0900

    commit

---
+++ user/src/main/java/project/domain/SubscriptionRequested.java
@@ -0,0 +1,24 @@
+package project.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import project.domain.*;
+import project.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class SubscriptionRequested extends AbstractEvent {
+
+    private Long id;
+
+    public SubscriptionRequested(User aggregate) {
+        super(aggregate);
+    }
+
+    public SubscriptionRequested() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
