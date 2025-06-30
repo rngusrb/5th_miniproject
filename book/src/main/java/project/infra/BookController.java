2c379f186830d2b07967fe437383e6d5cb485aa3
okjungwoo <okjungwoo@gmail.com>
2025-06-30 22:51:37 +0900

    controller

--- book/src/main/java/project/infra/BookController.java
+++ book/src/main/java/project/infra/BookController.java
@@ -20,36 +20,6 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
     
-    @GetMapping(path = "/aaa")
-    public String booksAaa() {
-        Book book = new Book();
-
-        book.PublishBook();
-        
-        return "test";
-    }
-
-
-    // @GetMapping(path = "/author/{authorId}")
-    // public List<BookDTO.Response> booksByAuthor(@PathVariable("authorId") Long authorId) {
-    //     List<Book> books = bookRepository.findByAuthorId(authorId);
-
-    //     return books.stream()
-    //             .map(BookDTO::toResponse)
-    //             .toList();
-    // }
-    
-    // @PatchMapping(path = "/{bookId}/view")
-    // public BookDTO.Response viewBook(@PathVariable("bookId") Long bookId) {
-    //     Book book = bookRepository.findById(bookId)
-    //             .orElseThrow(() -> new RuntimeException("도서를 찾을 수 없습니다."));
-
-    //     book.setViewCount(book.getViewCount() + 1);
-    //     bookRepository.save(book);
-
-    //     return BookDTO.toResponse(book);
-    // }
-
     @PatchMapping(
         value = "/{bookId}/viewbook",
         produces = "application/json;charset=UTF-8"
