428fd2dfbb941f52b4740e334820c21eaa866073
KOO HYEON GYU <147575574+rngusrb@users.noreply.github.com>
2025-07-01 10:45:19 +0900

    point 추가 메소드

--- point/src/main/java/project/infra/PointController.java
+++ point/src/main/java/project/infra/PointController.java
@@ -1,4 +1,5 @@
 package project.infra;
+import java.util.Date;
 
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
@@ -6,8 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import project.domain.*;
 
 //<<< Clean Arch / Inbound Adaptor
@@ -19,5 +19,29 @@ public class PointController {
 
     @Autowired
     PointRepository pointRepository;
+
+
+    @PutMapping("/{userId}/pluspoints")
+    public Point addPoints(
+        @PathVariable Long userId,
+        @RequestBody PointRequest request
+    ) {
+        Integer points = request.getPoints();
+
+        Long currentSum = 0L;
+        Point latest = pointRepository.findLatestByUserId(userId);
+        if (latest != null) {
+            currentSum = latest.getPointSum();
+        }
+
+        Point point = new Point();
+        point.setUserId(userId);
+        point.setChangeDate(new Date());
+        point.setChangePoint(points);
+        point.setPointSum(currentSum + points);
+        point.setReason("수동 충전");
+
+        return pointRepository.save(point);
+    }
 }
 //>>> Clean Arch / Inbound Adaptor
