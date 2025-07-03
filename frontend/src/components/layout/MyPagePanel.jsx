7b38322a8218c0768be26464834fb32d97ef7d52
hyunseong <dnclgk9@naver.com>
2025-07-03 14:08:38 +0900

    열람도서 버튼 삭제

--- frontend/src/components/layout/MyPagePanel.jsx
+++ frontend/src/components/layout/MyPagePanel.jsx
@@ -9,7 +9,6 @@ export default function MyPagePanel({ onClose, onChargeClick, onSubscribeClick, 
         <li onClick={onChargeClick}>포인트 충전</li>
         <li onClick={onSubscribeClick}>정기 구독권 관리</li> {/* ✅ 클릭 가능하게 수정 */}
         <li onClick={onMySubscribeClick}>구독중인 도서</li> 
-        <li>열람한 도서</li>
         <li onClick={onLogout}>로그아웃</li>
         <li onClick={onClose}>닫기</li>
       </ul>
