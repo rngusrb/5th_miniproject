5f1c785070bbcfdabfa2f19b4e3295824862772e
okjungwoo <okjungwoo@gmail.com>
2025-07-04 11:53:42 +0900

    axiosInstance baseURL 삭제

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,10 +2,11 @@ import axios from "axios";
 // 하드코딩된 주소 대신, 동적 설정 로직을 import 합니다.
 import baseURL from "../apiConfig";
 
-const axiosInstance = axios.create({
-  // 동적으로 결정된 baseURL을 사용
-  baseURL: baseURL,
-});
+// const axiosInstance = axios.create({
+//   // 동적으로 결정된 baseURL을 사용
+//   baseURL: baseURL,
+// });
+const axiosInstance = axios.create(); // baseURL 없이 생성
 
 axiosInstance.interceptors.request.use(
   (config) => {
